c416c9b9afc16d68c84c73d12c9253756d481680
Pilou <guillaume.beguin@gmail.com>
2016-03-15 10:39:48 +0100

    [DEV] Add back butten on browse screen

--- app/src/main/java/com/freak/videosenfants/BrowseDlnaActivity.java
+++ app/src/main/java/com/freak/videosenfants/BrowseDlnaActivity.java
@@ -86,8 +86,7 @@ public class BrowseDlnaActivity extends BrowseActivity implements AdapterView.On
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                BrowseDlnaActivity.this.onBackPressed();
             }
         });
 
