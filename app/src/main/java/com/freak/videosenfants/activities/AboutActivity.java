ac3195dee5a17f1b49fa9c7f7eb4427544676f8b
Pilou <guillaume.beguin@gmail.com>
2016-04-15 16:36:33 +0200

    [DEV] Add about screen

---
+++ app/src/main/java/com/freak/videosenfants/activities/AboutActivity.java
@@ -0,0 +1,30 @@
+package com.freak.videosenfants.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.freak.videosenfants.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView modulesView = (TextView) findViewById(R.id.modules);
+
+        String modules = getString(R.string.modules_list);
+        modules += "- Cling (http://4thline.org/projects/cling/)\n";
+        modules += "- FFmpeg (https://www.ffmpeg.org/)\n";
+        modules += "- FFmpegMediaMetadataRetriever (https://github.com/wseemann/FFmpegMediaMetadataRetriever)\n";
+        modules += "- httpcore (https://hc.apache.org/httpcomponents-core-ga/)\n";
+
+        modulesView.setText(modules);
+    }
+
+}
