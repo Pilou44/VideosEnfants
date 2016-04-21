e5688cff4ff84c390757c389bcb75ae86e9c805e
Pilou <guillaume.beguin@gmail.com>
2016-04-21 14:49:25 +0200

    [DEV] Use Universal Image Loader to manage thumbnails
    Store thumbnails in external cache

--- app/src/main/java/com/freak/videosenfants/elements/browsing/VideoElement.java
+++ app/src/main/java/com/freak/videosenfants/elements/browsing/VideoElement.java
@@ -3,9 +3,6 @@ package com.freak.videosenfants.elements.browsing;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -13,6 +10,8 @@ import android.util.Log;
 import com.freak.videosenfants.R;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import wseemann.media.FFmpegMediaMetadataRetriever;
 
@@ -23,7 +22,6 @@ public class VideoElement {
 
     private final Context mContext;
     private final boolean mDirectory;
-    private Drawable mIcon;
     private final String mPath;
     private String mName;
     private final VideoElement mParent;
@@ -54,10 +52,6 @@ public class VideoElement {
         return mDirectory;
     }
 
-    public Drawable getIcon() {
-        return mIcon;
-    }
-
     public String getPath() {
         return mPath;
     }
@@ -68,112 +62,6 @@ public class VideoElement {
     
     public VideoElement getParent() {
         return mParent;
-    }
-
-    private Bitmap getBitmapForElement() {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
-
-        File picturesLocation = new File(sharedPref.getString("local_pictures", mContext.getString(R.string.default_local_pictures)));
-        boolean locationExists = picturesLocation.exists();
-        if (!locationExists) {
-            if (DEBUG) {
-                Log.i(TAG, "Create directory: " + picturesLocation.getAbsolutePath());
-            }
-            locationExists = picturesLocation.mkdir();
-            if (DEBUG) {
-                Log.i(TAG, "Directory created: " + locationExists);
-            }
-
-        }
-
-        if (locationExists) {
-            if (DEBUG) {
-                Log.i(TAG, "Pictures location: " + picturesLocation.getAbsolutePath());
-            }
-
-            String fileNamePng = mName + ".png";
-            String fileNameJpg = mName + ".jpg";
-            String fileNameJpeg = mName + ".jpeg";
-
-            File picPng = new File(picturesLocation, fileNamePng);
-            File picJpg = new File(picturesLocation, fileNameJpg);
-            File picJpeg = new File(picturesLocation, fileNameJpeg);
-
-            if (picPng.exists()) {
-                return BitmapFactory.decodeFile(picPng.getAbsolutePath());
-            } else if (picJpg.exists()) {
-                return BitmapFactory.decodeFile(picJpg.getAbsolutePath());
-            } else if (picJpeg.exists()) {
-                return BitmapFactory.decodeFile(picJpeg.getAbsolutePath());
-            }
-        }
-        return null;
-    }
-
-    void generateScreenshot() {
-
-       if (mIcon == null) {
-            if (DEBUG)
-                Log.i(TAG, "Generate thumbnail for " + mName);
-
-           int pxWidth = mContext.getResources().getDimensionPixelSize(R.dimen.thumbnail_width);
-           int pxHeight = mContext.getResources().getDimensionPixelSize(R.dimen.thumbnail_height);
-
-           Bitmap bmp = getBitmapForElement();
-            if (bmp != null) {
-                int srcWidth = bmp.getWidth();
-                int srcHeight = bmp.getHeight();
-                int destWidth;
-                int destHeight;
-
-                destWidth = pxWidth;
-                destHeight = (srcHeight * pxWidth) / srcWidth;
-
-                if (destHeight > pxHeight){
-                    destHeight = pxHeight;
-                    destWidth = (srcWidth * pxHeight) / srcHeight;
-                }
-
-                mIcon = new BitmapDrawable(mContext.getResources(), Bitmap.createScaledBitmap(bmp, destWidth, destHeight, false));
-                bmp.recycle();
-            }
-            else {
-                if (DEBUG) {
-                    Log.i(TAG, "No local image found");
-                }
-                if (mDirectory) {
-                    mIcon = mContext.getDrawable(R.drawable.empty);
-                } else {
-                    if (DEBUG) {
-                        Log.i(TAG, "Try to extract thumbnail");
-                    }
-
-                    try {
-                        FFmpegMediaMetadataRetriever mmr = new FFmpegMediaMetadataRetriever();
-                        mmr.setDataSource(mPath);
-                        bmp = mmr.getFrameAtTime(120000000); // frame at 120 seconds
-                        mmr.release();
-                    }
-                    catch (IllegalArgumentException e) {
-                        Log.e(TAG, "Error ith source (" + mPath + ")");
-                        e.printStackTrace();
-                    }
-
-                    if (bmp == null) {
-                        mIcon = mContext.getResources().getDrawable(R.drawable.fichier, null);
-                    } else {
-                        mIcon = new BitmapDrawable(mContext.getResources(), ThumbnailUtils.extractThumbnail(bmp, pxWidth, pxHeight, ThumbnailUtils.OPTIONS_RECYCLE_INPUT));
-                    }
-
-                    if (DEBUG) {
-                        Log.i(TAG, "Thumbnail extraction finished");
-                    }
-                }
-            }
-        }
-        else if (DEBUG) {
-            Log.i(TAG, "Thumbnail has already been stored");
-        }
     }
 
     public void setSize(long size) {
@@ -192,4 +80,123 @@ public class VideoElement {
         return mPathFromRoot;
     }
 
+    public String getImageURI() {
+        String uri = getBitmapURI();
+        if (uri != null) {
+            return uri;
+        }
+        else if (!mDirectory) {
+            return getThumbnail();        }
+        else {
+            return null;
+        }
+    }
+
+    private String getThumbnail() {
+        File imageFile = new File(mContext.getExternalCacheDir(), mName + ".png");
+
+        if (DEBUG)
+            Log.i(TAG, "Thumbnail's path: " + imageFile.getAbsolutePath());
+
+        if (imageFile.exists()) {
+            if (DEBUG)
+                Log.i(TAG, "Thumbnail exists in cache");
+            return "file://" + imageFile.getAbsolutePath();
+        }
+        else {
+            if (DEBUG) {
+                Log.i(TAG, "Extract new thumbnail");
+            }
+
+            Bitmap bmp = null;
+            try {
+                FFmpegMediaMetadataRetriever mmr = new FFmpegMediaMetadataRetriever();
+                mmr.setDataSource(mPath);
+                bmp = mmr.getFrameAtTime(120000000); // frame at 120 seconds
+                mmr.release();
+            }
+            catch (IllegalArgumentException e) {
+                Log.e(TAG, "Error ith source (" + mPath + ")");
+                e.printStackTrace();
+            }
+
+            if (bmp == null) {
+                if (DEBUG) {
+                    Log.i(TAG, "Error extracting thumbnail");
+                }
+                return null;
+            } else {
+                int pxWidth = mContext.getResources().getDimensionPixelSize(R.dimen.thumbnail_width);
+                int pxHeight = mContext.getResources().getDimensionPixelSize(R.dimen.thumbnail_height);
+                bmp = ThumbnailUtils.extractThumbnail(bmp, pxWidth, pxHeight, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(imageFile);
+                    bmp.compress(Bitmap.CompressFormat.PNG, 80, fos);
+                    fos.close();
+                    bmp.recycle();
+                    if (DEBUG) {
+                        Log.i(TAG, "Thumbnail extracted");
+                    }
+                    return "file://" + imageFile.getAbsolutePath();
+                }
+                catch (IOException e) {
+                    if (DEBUG) {
+                        Log.i(TAG, "Error writing thumbnail");
+                    }
+                    Log.e(TAG, e.getMessage());
+                    if (fos != null) {
+                        try {
+                            fos.close();
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                    return null;
+                }
+            }
+
+
+        }
+
+    }
+
+    public String getBitmapURI() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
+
+        File picturesLocation = new File(sharedPref.getString("local_pictures", mContext.getString(R.string.default_local_pictures)));
+        boolean locationExists = picturesLocation.exists();
+        if (!locationExists) {
+            if (DEBUG) {
+                Log.i(TAG, "Create directory: " + picturesLocation.getAbsolutePath());
+            }
+            locationExists = picturesLocation.mkdir();
+            if (DEBUG) {
+                Log.i(TAG, "Directory created: " + locationExists);
+            }
+        }
+
+        if (locationExists) {
+            if (DEBUG) {
+                Log.i(TAG, "Pictures location: " + picturesLocation.getAbsolutePath());
+            }
+
+            String fileNamePng = mName + ".png";
+            String fileNameJpg = mName + ".jpg";
+            String fileNameJpeg = mName + ".jpeg";
+
+            File picPng = new File(picturesLocation, fileNamePng);
+            File picJpg = new File(picturesLocation, fileNameJpg);
+            File picJpeg = new File(picturesLocation, fileNameJpeg);
+
+            if (picPng.exists()) {
+                return "file://" + picPng.getAbsolutePath();
+            } else if (picJpg.exists()) {
+                return "file://" + picJpg.getAbsolutePath();
+            } else if (picJpeg.exists()) {
+                return "file://" + picJpeg.getAbsolutePath();
+            }
+        }
+        return null;
+    }
 }
