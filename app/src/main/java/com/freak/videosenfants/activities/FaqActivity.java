7edaae3f812f5cbb8cac81be80897ce1d89522cd
Pilou <guillaume.beguin@gmail.com>
2016-05-03 12:27:51 +0200

    [INTEGRATION] Code cleaning

--- app/src/main/java/com/freak/videosenfants/activities/FaqActivity.java
+++ app/src/main/java/com/freak/videosenfants/activities/FaqActivity.java
@@ -26,16 +26,22 @@ public class FaqActivity extends AppCompatActivity implements View.OnClickListen
         setSupportActionBar(toolbar);
 
         Button button1 = (Button) findViewById(R.id.configure_local);
+        assert button1 != null;
         button1.setOnClickListener(this);
         Button button2 = (Button) findViewById(R.id.configure_upnp);
+        assert button2 != null;
         button2.setOnClickListener(this);
         Button button3 = (Button) findViewById(R.id.copy_to_local);
+        assert button3 != null;
         button3.setOnClickListener(this);
         Button button4 = (Button) findViewById(R.id.find_thumbnail);
+        assert button4 != null;
         button4.setOnClickListener(this);
         Button button5 = (Button) findViewById(R.id.delete_local);
+        assert button5 != null;
         button5.setOnClickListener(this);
         Button button6 = (Button) findViewById(R.id.parent_mode);
+        assert button6 != null;
         button6.setOnClickListener(this);
     }
 
